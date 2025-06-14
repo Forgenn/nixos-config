1773fb38ae22aeeccbf879ffacfd25ece74fc71b
ntb <ipolmonxammar@gmail.com>
2025-06-14 21:10:32 +0200

    Set server side apply

--- hosts/revachol-cluster/dubois/charts/bootstrap-charts.nix
+++ hosts/revachol-cluster/dubois/charts/bootstrap-charts.nix
@@ -1,38 +1,38 @@
 {...}:
 {
       # If repos public, infisical/eso not needed on bootstrap
-      infisical = {
-        name = "infisical-standalone";
-        repo = "https://dl.cloudsmith.io/public/infisical/helm-charts/helm/charts/";
-        version = "1.5.0";
-        targetNamespace = "infisical";
-        createNamespace = true;
-        hash = "sha256-pASR3xWN6/6MK9KLTIMHvOq0fNjDZLvbWXSN+qSnQEI=";
-        values = {
-          infisical = {
-            image = {
-              repository = "infisical/infisical";
-              tag = "v0.131.0-postgres";
-              pullPolicy = "Always";
-            };
-          };
-          ingress = {
-            enabled = false;
-            nginx = {
-              enabled = false;
-            };
-          };
-        };
-      };
+      #infisical = {
+      #  name = "infisical-standalone";
+      #  repo = "https://dl.cloudsmith.io/public/infisical/helm-charts/helm/charts/";
+      #  version = "1.5.0";
+      #  targetNamespace = "infisical";
+      #  createNamespace = true;
+      #  hash = "sha256-pASR3xWN6/6MK9KLTIMHvOq0fNjDZLvbWXSN+qSnQEI=";
+      #  values = {
+      #    infisical = {
+      #      image = {
+      #        repository = "infisical/infisical";
+      #        tag = "v0.131.0-postgres";
+      #        pullPolicy = "Always";
+      #      };
+      #    };
+      #    ingress = {
+      #      enabled = false;
+      #      nginx = {
+      #        enabled = false;
+      #      };
+      #    };
+      #  };
+      #};
 
-      eso = {
-        name = "external-secrets";
-        repo = "https://charts.external-secrets.io";
-        version = "0.17.0";
-        hash = "sha256-+1zkgx7YFiEEeUO4UMBWVF0JU3QaSWsPFjgCPWjZwWI=";
-        targetNamespace = "external-secrets";
-        createNamespace = true;
-      };
+      #eso = {
+      #  name = "external-secrets";
+      #  repo = "https://charts.external-secrets.io";
+      #  version = "0.17.0";
+      #  hash = "sha256-+1zkgx7YFiEEeUO4UMBWVF0JU3QaSWsPFjgCPWjZwWI=";
+      #  targetNamespace = "external-secrets";
+      #  createNamespace = true;
+      #};
 
       argocd = {
         enable = true;
--- hosts/revachol-cluster/dubois/manifests/bootstrap-argocd-manifests.nix
+++ hosts/revachol-cluster/dubois/manifests/bootstrap-argocd-manifests.nix
@@ -60,6 +60,7 @@ in
                   };
                   syncOptions = [
                     "CreateNamespace=true"
+                    "ServerSideApply=true"
                   ];
                 };
               };
